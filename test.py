8397fcfa3157a5c4da5e0f5dc841e5553ba14ab2
geoffrey <sm129843@gmail.com>
2025-04-15 14:15:53 +0900

    quit button

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtCore import QCoreApplication
 
 class Myapp(QWidget):
 
@@ -10,10 +11,15 @@ class Myapp(QWidget):
     def initUI(self):
         self.setWindowTitle("My first Application")
 
-        self.move(300, 300)
-        self.resize(400, 200)
-        self.show()
+        btn = QPushButton('Quit', self)
+        btn.move(50, 50)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(QCoreApplication.instance().quit)
 
+        self.setWindowTitle("Quit button")
+
+        self.setGeometry(300, 300, 300, 300)
+        self.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
