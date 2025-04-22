35bd1931e3c53ee25f3162d6d04ca86e8c1c8203
geoffrey <sm129843@gmail.com>
2025-04-22 17:37:43 +0900

    지역 분할까지

--- functions/file_handler.py
+++ functions/file_handler.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from PyQt5.QtWidgets import *
 
 def read_excel_safely(file_path):
     """엑셀 파일 경로로부터 시트 목록을 안전하게 반환"""
@@ -10,3 +11,22 @@ def read_excel_safely(file_path):
         return xl.sheet_names
     except Exception as e:
         raise RuntimeError(f"엑셀 파일 읽기 오류: {str(e)}")
+    
+
+def set_columns(file_path, sheet_name):
+    try:
+        target_file = pd.read_excel(file_path, sheet_name = sheet_name)
+        print(target_file.columns)
+        return list(target_file.columns)
+    except Exception as e:
+        raise e
+    
+
+def divide_location(div_dict, label):
+    df = pd.read_excel(div_dict["file_path"], sheet_name = div_dict["sheet_name"])
+    df.astype(str)
+    com_name = div_dict['com_name']
+    share_num = div_dict['share_num']
+    address = div_dict["address"]
+    df[share_num] = df[share_num].astype(int)
+    
